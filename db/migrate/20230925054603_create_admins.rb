a206030e51dc430da9a891fd314ebf9d66965c46
ksjavali <kritkajavali@gmail.com>
2023-09-25 01:59:23 -0400

    added all classes

---
+++ db/migrate/20230925054603_create_admins.rb
@@ -0,0 +1,14 @@
+class CreateAdmins < ActiveRecord::Migration[6.1]
+  def change
+    create_table :admins do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+      t.string :phone_number
+      t.string :address
+      t.string :credit_number
+
+      t.timestamps
+    end
+  end
+end
