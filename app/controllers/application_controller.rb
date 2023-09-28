637236764e76209f4d953751f6b867586adff0b1
Skanda Shastry <sshastr4.ncsu.edu>
2023-09-28 14:29:30 -0400

    added authentication functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,39 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
+  helper_method :current_user
+  helper_method :current_order
+  helper_method :admin_user
+  helper_method :logged_in?
+  before_action :authorized
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
+  end
+
+  def admin_user
+    if session[:admin_id]
+      @admin_user ||= Admin.find(session[:admin_id])
+    else
+      @admin_user = nil
+    end
+  end
+
+  def logged_in?
+     !current_user.nil? or !admin_user.nil?
+  end
+  def authorized
+    redirect_to root_path unless logged_in?
+  end
+
+  def current_order
+    if !session[:order_id].nil?
+      Order.find(session[:order_id])
+    else
+      Order.new
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,12 @@ Rails.application.routes.draw do
   resources :passengers
   resources :admins
   root 'home#index'
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'passengers#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  resources :admin_sessions, only: [:new, :create, :destroy]
+  get 'admin_login', to: 'admin_sessions#new', as: 'admin_login'
+  get 'admin_logout', to: 'admin_sessions#destroy', as: 'admin_logout'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
