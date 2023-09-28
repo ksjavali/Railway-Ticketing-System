3a09e6a5f2dd019a1bf44c39c13a8ed46605f584
ksjavali <kritkajavali@gmail.com>
2023-09-28 00:44:40 -0400

    trains model updated

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
     belongs_to :passenger
     belongs_to :trains
+    validates :rating, presence: true , numericality: {greater_than: 0, only_integer: true, less_than:6}
+    validates :review, presence: true
 end
--- app/models/train.rb
+++ app/models/train.rb
@@ -1,2 +1,5 @@
 class Train < ApplicationRecord
+    has_many: passengers
+    has_many: tickets
+    has_many: reviews
 end
