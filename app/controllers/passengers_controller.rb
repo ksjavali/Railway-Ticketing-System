3130b42cb83f2f70f2315d7c1b481cfa08b7f4c0
asaxen24 <asaxen24@ncsu.edu>
2023-09-30 01:42:36 -0400

    passenger destroy user/admin

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -17,7 +17,7 @@ class PassengersController < ApplicationController
 
   # GET /passengers/new
   def new
-    if !logged_in?
+    if admin_user || !logged_in?
       @passenger = Passenger.new
     else
       redirect_to root_url
@@ -60,10 +60,11 @@ class PassengersController < ApplicationController
 
   # DELETE /passengers/1 or /passengers/1.json
   def destroy
+    session[:passenger_id]=nil
     @passenger.destroy
-
+    
     respond_to do |format|
-      format.html { redirect_to passengers_url, notice: "Passenger was successfully destroyed." }
+      format.html { redirect_to root_url, notice: "Passenger was successfully destroyed." }
       format.json { head :no_content }
     end
   end
