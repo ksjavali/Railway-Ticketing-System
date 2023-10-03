53570b9fce4ab36b43dd3c321e26d6e787a35809
ksjavali <kritkajavali@gmail.com>
2023-10-03 01:36:31 -0400

    Added validations for email, phone and credit card numbers

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,9 @@
 class Admin < ApplicationRecord
     has_secure_password
     validates :email, presence: true, uniqueness: true
+    validates :email, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
+    validates :password, presence: true, on: [new, create]
+    validates :credit_number, numericality: { only_numeric: true }, length: {is: 16, message: "must be 16 digits"}
+    validates :phone_number, length: {is: 10}
 
 end
